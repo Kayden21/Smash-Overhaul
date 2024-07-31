8c7a0b2e7e886ad511ba8f861da8fa8a7506e2b0
Kayden21 <90885641+Kayden21@users.noreply.github.com>
2024-07-31 15:29:29 -0500

    Update sonic.rs

--- sonic.rs
+++ sonic.rs
@@ -9,7 +9,7 @@ use {
     smashline::*,
 };
 
-unsafe extern "C" sonic_game_specialnhoming(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_specialnhoming(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
         MotionModule::set_rate(agent.module_accessor, 80.0);
@@ -33,7 +33,7 @@ if macros::is_excute(agent) {
 }
 }
 
-unsafe extern "C" sonic_game_specialsdashhop(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_specialsdashhop(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 1, 0, Hash40::new("hip"), 8.0, 90, 60, 0, 60, 3.0, 0.0, 1.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 6, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
         AttackModule::set_add_reaction_frame_revised(agent.module_accessor, /*ID*/ 0, /*Frames*/ 10.0, /*Unk*/ false);
@@ -45,7 +45,7 @@ unsafe extern "C" sonic_game_specialsdashhop(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_specialsspin(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_specialsspin(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
         macros::UNABLE_AREA(agent, *FIGHTER_AREA_KIND_TREAD_JUMP_CHECK);
@@ -75,7 +75,7 @@ unsafe extern "C" fn sonic_gimmickjump_game_fall(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attack13(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attack13(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 4.0);
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 0, 0, Hash40::new("top"), 6.2, 361, 119, 0, 50, 2.6, 0.0, 7.2, 2.2, None, None, None, 2.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
@@ -88,7 +88,7 @@ unsafe extern "C" sonic_game_attack13(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks3(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks3(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
     }
@@ -116,7 +116,7 @@ unsafe extern "C" sonic_game_attacks3(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks3hi(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks3hi(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
     }
@@ -144,7 +144,7 @@ unsafe extern "C" sonic_game_attacks3hi(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks3lw(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks3lw(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
     }
@@ -173,7 +173,7 @@ unsafe extern "C" sonic_game_attacks3lw(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attackhi3(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attackhi3(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 6.0);
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 0, 0, Hash40::new("top"), 2.0, 96, 100, 100, 0, 5.2, 0.0, 8.0, 4.0, None, None, None, 1.0, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
@@ -211,7 +211,7 @@ unsafe extern "C" sonic_game_attackhi3(agent: &mut L2CAgentBase) {
     macros::FT_MOTION_RATE(agent, 1.0);
 }
 
-unsafe extern "C" sonic_game_attacklw3(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacklw3(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 3.0);
     if macros::is_excute(agent) {
         JostleModule::set_status(agent.module_accessor, false);
@@ -229,7 +229,7 @@ unsafe extern "C" sonic_game_attacklw3(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attackdash(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attackdash(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 5.0);
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 0, 0, Hash40::new("kneel"), 8.4, 38, 63, 0, 91, 4.8, 5.5, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 2, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
@@ -248,7 +248,7 @@ unsafe extern "C" sonic_game_attackdash(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks4(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks4(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 13.0);
     if macros::is_excute(agent) {
         WorkModule::on_flag(agent.module_accessor, *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
@@ -265,7 +265,7 @@ unsafe extern "C" sonic_game_attacks4(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks4hi(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks4hi(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 13.0);
     if macros::is_excute(agent) {
         WorkModule::on_flag(agent.module_accessor, *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
@@ -283,7 +283,7 @@ unsafe extern "C" sonic_game_attacks4hi(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attacks4lw(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attacks4lw(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 13.0);
     if macros::is_excute(agent) {
         WorkModule::on_flag(agent.module_accessor, *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
@@ -301,7 +301,7 @@ unsafe extern "C" sonic_game_attacks4lw(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" sonic_game_attackhi4(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sonic_game_attackhi4(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 3.0);
     macros::FT_MOTION_RATE(agent, 0.6);
     frame(agent.lua_state_agent, 6.0);
